690074134087a834e8b32848afc0364eea2dfa21
SkoomaFiend <realmofthemadgodac@gmail.com>
2025-03-08 17:42:41 -0500

    Create gitfiles.sh

---
+++ gitfiles.sh
@@ -0,0 +1,3 @@
+curl -O https://raw.githubusercontent.com/SkoomaFiend/DOTFILES/refs/heads/main/nvme.dump
+curl -O https://raw.githubusercontent.com/SkoomaFiend/DOTFILES/refs/heads/main/my-archinstall.sh
+curl -O https://raw.githubusercontent.com/SkoomaFiend/DOTFILES/refs/heads/main/install-in-root.sh
