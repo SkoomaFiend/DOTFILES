b8ea71dc521b4eb5158f7fa42ef12db5b1d12a2d
SkoomaFiend <realmofthemadgodac@gmail.com>
2025-03-08 17:03:33 -0500

    Create install-in-root.sh

---
+++ install-in-root.sh
@@ -0,0 +1,14 @@
+ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
+hwclock --systohc
+locale-gen
+touch /etc/locale.conf
+echo LANG=en_US.UTF-8 > /etc/locale.conf
+touch /etc/hostname
+echo dan > /etc/hostname
+mkinitcpio -P
+pacman -S grub efibootmgr --noconfirm
+mkdir /boot/EFI
+mount /dev/nvme0n1p1 /boot/EFI
+grub-install --target=x86_64-efi --efi-directory=/boot/EFI --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+useradd -m dan
