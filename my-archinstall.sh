e6c8c69603f2e0598cac11978a35d06fa7976335
SkoomaFiend <realmofthemadgodac@gmail.com>
2025-03-08 17:02:40 -0500

    Update my-archinstall.sh

--- my-archinstall.sh
+++ my-archinstall.sh
@@ -8,29 +8,6 @@ mount /dev/nvme0n1p2 /mnt
 mkdir /mnt/home/
 mount /dev/nvme0n1p3 /mnt/home
 swapon /dev/nvme0n1p4
-pacstrap -K /mnt base linux linux-firmware
+pacstrap -K /mnt base linux-zen linux-firmware
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
-ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
-hwclock --systohc
-locale-gen
-touch /etc/locale.conf
-echo LANG=en_US.UTF-8 > /etc/locale.conf
-touch /etc/hostname
-echo dan > /etc/hostname
-mkinitcpio -P
-pacman -S grub efibootmgr
-mkdir /boot/EFI
-mount /dev/nvme0n1p1 /boot/EFI
-
-
-
-
-
-
-
-
-
-
-
-
