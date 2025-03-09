da17de4783a0ae343574f0e2de31481d3e3764b7
SkoomaFiend <realmofthemadgodac@gmail.com>
2025-03-09 10:02:34 -0400

    Update install-in-root.sh

--- install-in-root.sh
+++ install-in-root.sh
@@ -6,7 +6,7 @@ echo LANG=en_US.UTF-8 > /etc/locale.conf
 touch /etc/hostname
 echo dan > /etc/hostname
 mkinitcpio -P
-pacman -S grub efibootmgr --noconfirm
+pacman -S grub efibootmgr os-prober networkmanager dialog --noconfirm
 mkdir /boot/EFI
 mount /dev/nvme0n1p1 /boot/EFI
 grub-install --target=x86_64-efi --efi-directory=/boot/EFI --bootloader-id=GRUB
