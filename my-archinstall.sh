7f6db699f8e456d20587e22eb30c15b8991928f8
SkoomaFiend <realmofthemadgodac@gmail.com>
2025-03-09 08:30:40 -0400

    Update my-archinstall.sh

--- my-archinstall.sh
+++ my-archinstall.sh
@@ -11,4 +11,4 @@ mount /dev/nvme0n1p3 /mnt/home
 swapon /dev/nvme0n1p4
 pacstrap -K /mnt base linux linux-firmware
 genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt
+arch-chroot /mnt /bin/bash -c "./install-in-root.sh"
