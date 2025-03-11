193af15ef36ae6472522611348008d19bddd23d6
SkoomaFiend <realmofthemadgodac@gmail.com>
2025-03-11 13:50:50 -0400

    Update gitfiles.sh

--- gitfiles.sh
+++ gitfiles.sh
@@ -1,4 +1,5 @@
 curl -O https://raw.githubusercontent.com/SkoomaFiend/DOTFILES/refs/heads/main/nvme.dump
 curl -O https://raw.githubusercontent.com/SkoomaFiend/DOTFILES/refs/heads/main/my-archinstall.sh
 curl -O https://raw.githubusercontent.com/SkoomaFiend/DOTFILES/refs/heads/main/install-in-root.sh
+curl -O https://raw.githubusercontent.com/SkoomaFiend/DOTFILES/refs/heads/main/TODO.md
 chmod +x my-archinstall.sh install-in-root.sh
